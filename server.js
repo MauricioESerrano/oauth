b033a9d7112bde9123a67cb974d1843c948ce0a5
MauricioESerrano <mauryeserrano@hotmail.com>
2025-02-11 14:27:22 -0800

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.AUTH0_CLIENT_SECRET,
-  baseURL: "137.110.115.26",
+  baseURL: "137.110.115.26:3000",
   clientID: process.env.AUTH0_CLIENT_ID,
   issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
   authorizationParams: {
